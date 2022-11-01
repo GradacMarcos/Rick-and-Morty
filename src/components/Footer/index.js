dbd73a07d22664a72a29b1ec84afcc416c2ab4c1
rojomaldit-jr <marcosgradac2022@gmail.com>
2022-11-01 20:07:18 -0300

    se le agrego efecto y funcion de click a redes y tambien un href para redireccionar a al contacto correspondiente

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,22 @@
-import { Grid, Typography } from "@mui/material";
+import { CardActionArea, Grid, Typography } from "@mui/material";
 import "./footer.scss";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import PhoneIcon from "@mui/icons-material/Phone";
-import EmailIcon from '@mui/icons-material/Email';
+import EmailIcon from "@mui/icons-material/Email";
 
-export default function Footer() {
-  const logoImgSrc =
-    "https://images-ext-1.discordapp.net/external/v4DAsbH0L8AVWPvOA9eAZwOgNcDpaDDONOhZIcqGBpI/https/www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-rick-face-kushmastafresh-deviantart-19.png?width=395&height=457";
+export default function Footer(props) {
+  const onClick = props;
   return (
     <Grid className="Footer">
       <Grid className="logo" container>
         <Grid item xs={4}>
-          <img className="logo" src={logoImgSrc}></img>
+          <img
+            className="logo"
+            src="https://images-ext-1.discordapp.net/external/v4DAsbH0L8AVWPvOA9eAZwOgNcDpaDDONOhZIcqGBpI/https/www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-rick-face-kushmastafresh-deviantart-19.png?width=395&height=457"
+            alt=""
+          ></img>
           <Grid container>
             <Grid item xs={1}>
               <PhoneIcon color="secondary" />
@@ -27,7 +30,9 @@ export default function Footer() {
               <EmailIcon color="secondary" />
             </Grid>
             <Grid item xs={11}>
-              <Typography variant="subtitle1">marcosgradac2022@gmail.com</Typography>
+              <Typography variant="subtitle1">
+                marcosgradac2022@gmail.com
+              </Typography>
             </Grid>
           </Grid>
         </Grid>
@@ -38,9 +43,32 @@ export default function Footer() {
         </Grid>
         <Grid className="redes" item xs={5}>
           <Typography variant="h4">REDES SOCIALES</Typography>
-          <FacebookIcon sx={{ fontSize: 40 }} color="secondary" />
-          <WhatsAppIcon sx={{ fontSize: 40 }}color="secondary" />
-          <InstagramIcon  sx={{ fontSize: 40 }}color="secondary" />
+          <Grid container>
+            <Grid item xs={3}>
+              <CardActionArea
+                onClick={onClick}
+                href="https://www.facebook.com/marcos.gradac.37/"
+              >
+                <FacebookIcon sx={{ fontSize: 40 }} color="secondary" />
+              </CardActionArea>
+            </Grid>
+            <Grid item xs={3}>
+              <CardActionArea
+                onClick={onClick}
+                href="https://web.whatsapp.com/"
+              >
+                <WhatsAppIcon sx={{ fontSize: 40 }} color="secondary" />
+              </CardActionArea>
+            </Grid>
+            <Grid item xs={3}>
+              <CardActionArea
+                onClick={onClick}
+                href="https://www.instagram.com/marcosgradac/"
+              >
+                <InstagramIcon sx={{ fontSize: 40 }} color="secondary" />
+              </CardActionArea>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
